1382470a853b4cc102575df52f8063844cbe9a24
Egor Zhilinsky <zhilinskyegor@gmail.com>
2015-08-26 05:45:31 +0300

    add a test

--- tests.rb
+++ tests.rb
@@ -7,7 +7,6 @@ RSpec.describe do
   end
 
   it "base_two_neg_reverser should convert 0 to 0" do
-                                 # => 0       # => -0
     expect(base_two_neg_reverser([0])).to eq([0])
   end
 
@@ -16,29 +15,34 @@ RSpec.describe do
     expect(base_two_neg_reverser([1])).to eq([1,1])
   end
 
+  it "base_two_neg_reverser should convert 4 to -4" do
+                                 # => 4            # => -4
+    expect(base_two_neg_reverser([0, 0, 1])).to eq([0, 0, 1, 1])
+  end
+
   it "base_two_neg_reverser should convert 9 to -9" do
-                            # => 9                    # => -9
+                                 # => 9                  # => -9
     expect(base_two_neg_reverser([1, 0, 0, 1, 1])).to eq([1, 1, 0, 1])
   end
 
   it "base_two_neg_reverser should convert -23 to 23" do
-                            # => -23                     # => 23
+                                  # => -23                  # => 23
     expect(base_two_neg_reverser([1, 0, 0, 1, 1, 1])).to eq([1, 1, 0, 1, 0, 1, 1])
   end
 
   it "base_two_neg_reverser should convert 255 to -255" do
-                            # => 255                     # => -255
+                                  # => 255                            # => -255
     expect(base_two_neg_reverser([1, 1, 0, 0, 0, 0, 0, 0, 1])).to eq([1, 0, 0, 0, 0, 0, 0, 0, 1, 1])
   end
 
   it "base_two_neg_reverser should convert -256 to 256" do
-                            # => -256                     # => 256
+                                  # => -256                             # => 256
     expect(base_two_neg_reverser([0, 0, 0, 0, 0, 0, 0, 0, 1, 1])).to eq([0, 0, 0, 0, 0, 0, 0, 0, 1])
   end
 
 
   it "base_two_neg_reverser should convert 43654 to -43654" do
-#                          # => 43654                                                                                                            # => -43654
+                                 # => 43654                                                                                            # => -43654
     expect(base_two_neg_reverser([0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1])).to eq([0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1])
   end
 
