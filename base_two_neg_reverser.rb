66757f685712c328718ea7a6b693e639ffc1da47
Egor Zhilinsky <zhilinskyegor@gmail.com>
2015-08-26 05:37:44 +0300

    add comments to the method

--- base_two_neg_reverser.rb
+++ base_two_neg_reverser.rb
@@ -1,33 +1,37 @@
 def base_two_neg_reverser(array)
 
-  # => The empty sequence represents 0.
+  # => The empty sequence and [0] represents [0].
   if array.empty? || array == [0]
     return [0]
   end
 
-  change_next_element = false
   result = []
+  change_next_element = false
   array.each_with_index do | element, index |
 
     result[index] = element
 
+    # => if input number is negative we have to make it positive(add the biggest rank)
     if element == 1 && index == array.size - 1 && change_next_element == false
       result << 1
       break
     end
 
+    # => if previous element was 1 we have to change current element
     if change_next_element == true
       result[index] ^= 1
       change_next_element = false
       next
     end
 
+    # => if current el is 1 we have to change next
     if element == 1
       change_next_element = true
     end
 
   end
 
+  # => if last element will be 0 can make the result smaller
   result.pop if result[-1] == 0
 
   result
